d699aa202247e7ef92e0970ae2ce6bf2f515fab9
emmanuel ah-hong <emmanuel.a@lycee-de-beauvoir.education>
2024-12-10 17:48:43 +0100

    Commit 1

---
+++ xxxx_plantes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+
+int main()
+{
+    stdio_init_all();
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
